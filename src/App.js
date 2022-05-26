0aacc8d891e925ee0eaaa714650bb728617db707
Sahitya77 <sahityanijhawan@gmail.com>
2022-05-26 19:51:31 +0530

    personal portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route } from 'react-router-dom'
+import GlobalStyle from './GlobalStyles'
+// import {ThemeProvider} from "styled-components"
+// import { lightTheme } from './components/Themes'
+import Main from './components/Main';
+import AboutPage from './components/AboutPage';
+import BlogPage from './components/BlogPage';
+import WorkPage from './components/WorkPage';
+import MySkillsPage from './components/MySkillsPage';
+import { Switch } from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GlobalStyle/>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/blog" component={BlogPage} />
+          <Route exact path="/work" component={WorkPage} />
+          <Route exact path="/skills" component={MySkillsPage} />
+        </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
