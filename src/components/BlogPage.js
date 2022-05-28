10cb38815828104f2b4f57c9d258716b88d28f77
Sahitya77 <sahityanijhawan@gmail.com>
2022-05-28 14:34:59 +0530

    blog component

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,8 +1,58 @@
 import React from 'react'
+import styled from 'styled-components'
+import img from '../assets/Images/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg'
+import LogoComponent from '../subComponents/LogoComponent'
+import PowerButton from '../subComponents/PowerButton'
+import SocialIcons from '../subComponents/SocialIcons'
+import {Blogs} from '../data/BlogData';
+import BlogComponent from './BlogComponent'
 
+const MainContainer=styled.div`
+  background-image:url(${img});
+  background-size:cover;
+  background-repeat:no-repeat;
+  background-attachment;fixed;
+  background-position:center;
+
+`
+const Container=styled.div`
+  background-color:rgba(252, 246, 244,0.8);
+  width:100%;
+  height:auto;
+  position:relative;
+  padding-bottom:5rem;
+`
+const Center =styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  padding-top:10rem;
+
+`
+const Grid=styled.div`
+  display:grid;
+  grid-template-columns: repeat(2,minmax(calc(10rem + 15vw),1fr));
+  grid-gap:calc(1rem + 2vw);
+
+`
 function BlogPage() {
   return (
-    <div>BlogPage</div>
+    <MainContainer>
+      <Container>
+        <LogoComponent/>
+        <PowerButton/>
+        <SocialIcons/>
+        <Center>
+          <Grid>
+            {
+              Blogs.map(blog=>{
+                return <BlogComponent key={blog.id} blog={blog}/>
+              })
+            }
+          </Grid>
+        </Center>
+      </Container>
+    </MainContainer>
   )
 }
 
