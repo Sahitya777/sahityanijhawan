e1a5e69d37d3daac49f5bc7d33632dd6df4d9d45
Sahitya77 <sahityanijhawan@gmail.com>
2022-05-28 15:36:30 +0530

    anchor updates

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -6,6 +6,7 @@ import PowerButton from '../subComponents/PowerButton'
 import SocialIcons from '../subComponents/SocialIcons'
 import {Blogs} from '../data/BlogData';
 import BlogComponent from './BlogComponent'
+import AnchorComponent from '../subComponents/Anchor'
 
 const MainContainer=styled.div`
   background-image:url(${img});
@@ -42,6 +43,7 @@ function BlogPage() {
         <LogoComponent/>
         <PowerButton/>
         <SocialIcons/>
+        <AnchorComponent/>
         <Center>
           <Grid>
             {
--- src/subComponents/Anchor.js
+++ src/subComponents/Anchor.js
@@ -1,11 +1,62 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { Link } from '../components/Allsvgs'
+import styled from "styled-components"
+import { Anchor } from '../components/Allsvgs'
+const Container=styled.div`
+    position:relative;
+`
+const Slider=styled.div`
+    position:fixed;
+    top:0;
+    right:2rem;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    flex-direction:column;
+    transform: translateY(-100%);
 
-function Anchor() {
+    .chain{
+        transform: rotate(135deg);
+    }
+
+`
+function AnchorComponent() {
+
+    const ref=useRef(null);
+    const hiddenRef=useRef(null);
+
+    useEffect(()=>{
+
+        const handleScroll=()=>{
+            let scrollPosition=window.pageYOffset;
+            let windowSize=window.innerHeight;
+            let bodyHeight=document.body.offsetHeight;
+
+            let diff=Math.max(bodyHeight-(scrollPosition+windowSize));
+            
+            let diffP=(diff * 100)/(bodyHeight-windowSize);
+
+            ref.current.style.transform= `translateY(${-diffP}%)`
+        }
+
+        window.addEventListener('scroll',handleScroll)
+        return()=>window.removeEventListener('scroll',handleScroll)
+    },[])
+
   return (
-    <div>
-        Anchor
-    </div>
+    <Container>
+        {/* <PreDisplay ref={hiddenRef} className='hidden'/> */}
+        <Anchor width={70} height={70} fill='currentColor' />
+        <Slider ref={ref}>
+            {
+                [...Array(25)].map((x,id)=>{
+                    return <Link key={id} width={25} height={25} fill='currentColor' className='chain' />
+                })
+            }
+            <Anchor width={70} height={70} fill='currentColor'/>
+        </Slider>
+    </Container>
   )
 }
 
-export default Anchor
+export default AnchorComponent
