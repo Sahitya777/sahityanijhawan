e06beca144d94e797f6e0112695b81bfedd053c3
Sahitya77 <sahityanijhawan@gmail.com>
2022-05-29 14:55:01 +0530

    skills page updates

--- src/components/MySkillsPage.js
+++ src/components/MySkillsPage.js
@@ -1,8 +1,57 @@
 import React from 'react'
+import {ThemeProvider} from 'styled-components'
+import {lightTheme} from './Themes'
+import { Design, Develope } from './Allsvgs'
+import styled from 'styled-components'
+const Box=styled.div`
+  background-color:${props=>props.theme.body};
+  width:100vw;
+  height:100vh;
+  position:relative;
+  display:flex;
+  justify-content:space-evenly;
+  align-items:center;
 
-function MySkillsPage() {
+`
+const Main=styled.div`
+  border:2px solid black;
+  color:black;
+  background-color:white;
+  padding: 2rem;
+  width:30vw;
+  height:60vh;
+  z-index:3;
+  line-height:1.5;
+
+  font-family:'Ubuntu Mono',monospace;
+  display:flex;
+  flex-direction:column;
+  justify-content:space-between;
+
+`
+const Title=styled.h2`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  font-size:calc(1em + 1vw);
+`
+const MySkillsPage=()=> {
   return (
-    <div>MySkills</div>
+    <ThemeProvider theme={lightTheme}>
+      <Box>
+        <Main>
+          <Title>
+            <Design width={40} height={40} /> Mobile Developer
+          </Title>
+        </Main>
+        <Main>
+          <Title>
+          <Develope width={40} height={40} /> Frontend Developer
+          </Title>
+        </Main>
+      </Box>
+    </ThemeProvider>
+    
   )
 }
 
