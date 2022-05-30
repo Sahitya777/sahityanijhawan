d443e2f51e4fd6a19d4813cae62889b9de18668e
Sahitya77 <sahityanijhawan@gmail.com>
2022-05-30 22:55:29 +0530

    some minor changes

--- src/components/WorkPage.js
+++ src/components/WorkPage.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import styled, {ThemeProvider } from 'styled-components'
+import {DarkTheme} from './Themes';
 
-function WorkPage() {
-  return (
-    <div>WorkPage</div>
-  )
+
+import LogoComponent from '../subComponents/LogoComponent';
+import SocialIcons from '../subComponents/SocialIcons';
+import PowerButton from '../subComponents/PowerButton';
+
+import {Work} from '../data/WorkData'
+
+const Box = styled.div`
+background-color: black;
+width: 100vw;
+height:100vh;
+position: relative;
+overflow: hidden;
+`
+
+
+
+
+
+const WorkPage = () => {
+    return (
+        <ThemeProvider theme={DarkTheme}>
+<Box>
+
+<LogoComponent theme='dark'/>
+<SocialIcons theme='dark'/>
+<PowerButton />
+
+
+        
+
+
+        </Box>
+
+        </ThemeProvider>
+        
+    )
 }
 
 export default WorkPage
