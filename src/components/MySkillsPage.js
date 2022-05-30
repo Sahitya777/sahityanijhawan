624a41b398c9bffcc05cfe042e60fe5ec22ec2e6
Sahitya77 <sahityanijhawan@gmail.com>
2022-05-30 15:28:08 +0530

    skill section complete

--- src/components/MySkillsPage.js
+++ src/components/MySkillsPage.js
@@ -3,6 +3,12 @@ import {ThemeProvider} from 'styled-components'
 import {lightTheme} from './Themes'
 import { Design, Develope } from './Allsvgs'
 import styled from 'styled-components'
+import LogoComponent from '../subComponents/LogoComponent'
+import SocialIcons from '../subComponents/SocialIcons';
+import PowerButton from '../subComponents/PowerButton';
+import ParticleComponent from '../subComponents/ParticleComponent'
+
+
 const Box=styled.div`
   background-color:${props=>props.theme.body};
   width:100vw;
@@ -16,17 +22,21 @@ const Box=styled.div`
 const Main=styled.div`
   border:2px solid black;
   color:black;
-  background-color:white;
+  background-color:${props=>props.theme.body};
   padding: 2rem;
   width:30vw;
   height:60vh;
   z-index:3;
   line-height:1.5;
-
-  font-family:'Ubuntu Mono',monospace;
+  cursor:pointer;
+  font-family: 'Ubuntu Mono',monospace;
   display:flex;
   flex-direction:column;
   justify-content:space-between;
+  &:hover{
+    color:${props=>props.theme.body};
+    background-color:${props=>props.theme.text};
+  }
 
 `
 const Title=styled.h2`
@@ -34,20 +44,96 @@ const Title=styled.h2`
   justify-content:center;
   align-items:center;
   font-size:calc(1em + 1vw);
+  ${Main}:hover &{
+    &>*{
+      fill:${props=>props.theme.body};
+
+    }
+  }
+  &*>:first-child{
+    margin-left: 1rem;
+
+  }
+`
+const Description=styled.div`
+  color:${props=>props.theme.text};
+  font-size:calc(0.6em + 1vw);
+  padding:0.5rem 0;
+  font-family:'Ubuntu Mono',monospace;
+  ${Main}:hover &{
+    
+      color:${props=>props.theme.body};
+
+    
+  strong{
+    margin-bottom: 1rem;
+    text-transform: uppercase;
+
+  }
+  ul,p{
+    margin-left: 2rem;
+  }
+
 `
 const MySkillsPage=()=> {
   return (
     <ThemeProvider theme={lightTheme}>
       <Box>
+        <LogoComponent theme='dark'/>
+        <SocialIcons theme='light'/>
+        <PowerButton/>
+        <ParticleComponent theme='light'/>
         <Main>
           <Title>
             <Design width={40} height={40} /> Mobile Developer
           </Title>
+          <Description>
+            I love to make clean and simple mobile apps. 
+          </Description>
+          <Description>
+            <strong>
+              I like to Develop
+            </strong>
+            <ul>
+              <li>
+                Mobile Apps
+              </li>
+            </ul>
+          </Description>
+          <Description>
+            <strong>
+              Tools
+            </strong>
+            <ul>
+              <li>
+                Android Studio
+              </li>
+            </ul>
+          </Description>
         </Main>
         <Main>
           <Title>
-          <Develope width={40} height={40} /> Frontend Developer
+            <Develope width={40} height={40} /> Frontend Developer
           </Title>
+          <Description>
+            I value buisness or brand for which i'm creating, thus i enjoy bringing new ideas to life. 
+          </Description>
+          <Description>
+            <strong>
+              Skills
+            </strong>
+            <p>
+              Html, Css, Js, React, Redux, Sass, FireBase, MongoDb, SQL etc.
+            </p>
+          </Description>
+          <Description>
+            <strong>
+              Tools
+            </strong>
+            <p>
+              Vscode, Github, Codepen etc.
+            </p>
+          </Description>
         </Main>
       </Box>
     </ThemeProvider>
